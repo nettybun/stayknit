8b9a182a12da13c5e922f34b22913c501d62215c
Gen Hames <hamesg@tutanota.de>
2020-06-04 23:06:46 -0700

    Use `El` over Node. Data integrity issues. Loop issues...
    Learned WeakSet/WeakMap aren't iterable. That sucks.

--- src/trace.tsx
+++ src/trace.tsx
@@ -1,18 +1,20 @@
 import type { h, SinuousApi } from 'sinuous/jsx';
 
-// Typescript isn't very strict, I don't use `extend`/`&` because it seems to
-// widen types on arrays:
-
-// type X = { x?: string };
-// let a: X[] = [];
-// let b: X & { a: string } = { a: '' }
-// a.push(b); (No error, even though I said only allow { x })
-
-// Possibly related:
-// https://mariusschulz.com/blog/weak-type-detection-in-typescript#the-limits-of-weak-type-detection
-
 enum ComponentNameBrand { _ = '' }
 type ComponentName = ComponentNameBrand & string;
+
+// TODO:
+// Tree uses Node.closest to search for parents, but this opens a potential for
+// data inconsistency - see tree.isComponent. This could be resolved by instead
+// walking the DOM via Node.parentNode and testing tree.cInstanceDetails.has()
+
+/** El.dataset[DATASET_TAG] = ComponentName; Also as <h1 data-DATASET_TAG /> */
+const DATASET_TAG = 'component';
+
+type El = (HTMLElement | SVGElement)
+        & { dataset: { [DATASET_TAG]?: ComponentName } & DOMStringMap };
+
+type Component = El;
 
 type LifecycleNames =
   | 'onAttach'
@@ -25,73 +27,113 @@ type LifecycleMethods = {
 /** Instance information */
 type InstanceDetails = {
   name: ComponentName;
-  node: Node;
-  attached: boolean;
+  el: El;
+  isDOMBridge: boolean;
   // TODO: Add timing, rerender count, etc
 } & LifecycleMethods;
 
-const tree = {
+const treeDS = {
   /** Collection area for lifecycle methods set during render */
   cRenderingStack: [] as (LifecycleMethods & { name: ComponentName })[],
-  /** Map a given instance (DOM node) to component information */
-  cInstanceDetails: new WeakMap<Node, InstanceDetails>(),
-  /** Map a component name to all of its instances (DOM nodes) */
-  cInstances: new Map<ComponentName, WeakSet<Node>>(),
-  /** _cPO.get(Child) => Parent */
-  cParentOf: new WeakMap<Node, Node>(),
-  /** _cCO.get(Parent) => WS[Child, Child, ...] */
-  cChildrenOf: new WeakMap<Node, WeakSet<Node>>(),
-  /** _cAP(Node) => <body/> (use cIL(Node) => <App/> to see component name) */
-  cRootMounts: new WeakMap<Node, Node>(),
+  /** Map a given instance (DOM element) to component information */
+  cInstanceDetails: new WeakMap<Component, InstanceDetails>(),
+  /** Map a component name to all of its instances (DOM elements) */
+  cInstances: new Map<ComponentName, WeakSet<Component>>(),
 
-  /** New component creation */
-  createInstance(componentName: ComponentName, node: Node) {
-    const instances = tree.cInstances.get(componentName) ?? new WeakSet<Node>();
-    tree.cInstances.set(componentName, instances.add(node));
+  // There has to be a way to hop between parent/child components. Node.closest
+  // works for parents, but Node.querySelector isn't appropriate for children
+  // because there's no depth limit - you'll get a flat list of all nested
+  // children. Have to store these relationships without walking the DOM:
+  /** Child => Parent */
+  cParentOf: new WeakMap<Component, Component>(),
+  /** Parent => WS[Child, Child, ...] */
+  cChildrenOf: new WeakMap<Component, WeakSet<Component>>(),
+
+  /** Elements that don't have a component parent (could have a DOM parent) */
+  cEntrypoints: new WeakSet<Component>(),
+};
+
+const treeInternalMethods = {
+
+  create(componentName: ComponentName, el: El) {
+    el.dataset[DATASET_TAG] = componentName;
+    const instances = tree.cInstances.get(componentName) ?? new WeakSet<El>();
+    tree.cInstances.set(componentName, instances.add(el));
+    tree.cEntrypoints.add(el);
   },
-  /** Attach components (doesn't have to be passed components) */
-  relate(parent: Node, child: Node) {
-    // TODO: Resolve nodes to closest component
+
+  isComponent(el: El) {
+    const inTree = tree.cInstanceDetails.has(el);
+    const inElementDataset = DATASET_TAG in el.dataset;
+    if (inTree && !inElementDataset) {
+      console.warn('Component in cInstanceDetails but wasn\'t tagged', el);
+      return false;
+    }
+    if (!inTree && inElementDataset) {
+      console.warn('Component was tagged but wasn\'t in cInstanceDetails', el);
+      return false;
+    }
+    return inTree && inElementDataset;
+  },
+
+  isAttached(comp: Component) {
+    if (!tree.isComponent(comp))
+      throw `Wasn't passed a component: tree.isAttached(${String(comp)}`;
+    return document.body.contains(comp);
+  },
+
+  relate(parent: Component, child: Component) {
+    if (!tree.isComponent(parent) || !tree.isComponent(child))
+      throw `Wasn't passed components: tree.relate(${String(parent)}, ${String(child)})`;
+
     tree.cParentOf.set(child, parent);
-    const children = tree.cChildrenOf.get(parent) ?? new WeakSet<Node>();
+    const children = tree.cChildrenOf.get(parent) ?? new WeakSet<El>();
     tree.cChildrenOf.set(parent, children.add(child));
   },
-  /** Determine attachment (doesn't have to be passed a component) */
-  isNodeAttached(node: Element) {
-    // Resolve nodes to closest component
-    let comp = undefined;
-    if (node instanceof HTMLElement || node instanceof SVGElement) {
-      comp = node.dataset.component
-        ? node as Node
-        : node.closest('[data-component]') as Node;
-    }
-    // Resolution failed, not in any component
-    if (!comp) return false;
 
-    let compPrev: Node;
-    do {
-      const data = tree.cInstanceDetails.get(comp);
-      if (!data)
-        throw `cPO>cID was undefined for ${String(node)}`;
-      if (data.attached)
-        return true;
-    // eslint-disable-next-line no-cond-assign
-    } while (compPrev = comp, comp = tree.cParentOf.get(comp));
-
-    // Made it this far, is the parent mounted?
-    return tree.cRootMounts.has(compPrev);
+  /** Finds closest component; inclusive */
+  nearestParent(el: Component) {
+    return tree.isComponent(el)
+      ? el
+      : el.closest(`[data-${DATASET_TAG}]`) as El;
   },
 
+  /** Find the entrypoint component that is attached to the DOM tree */
+  searchForDOMBridge(comp: Component) {
+    if (!tree.isComponent(comp))
+      throw `Wasn't passed a component: tree.searchForDOMBridge(${String(comp)}`;
+
+    let cur: Component | undefined = comp;
+    while (cur) {
+      const data = tree.cInstanceDetails.get(cur);
+      if (!data)
+        throw `cInstanceDetails wasn't defined for ${String(cur)}`;
+      if (data.isDOMBridge) {
+        if (!tree.cEntrypoints.has(cur))
+          throw `isDOMBridge but not in cEntrypoint: ${String(cur)}`;
+        return true;
+      }
+
+      const compMaybe = tree.cParentOf.get(cur);
+      // If at an entrypoint, sanity check (while in development)
+      if (!compMaybe && !tree.cEntrypoints.has(cur))
+        throw `Top of cParentOf tree wasn't in cEntrypoint: ${String(cur)}`;
+
+      cur = compMaybe;
+    }
+    return false;
+  },
+};
+
+const treeLifecycleMethods = {
   /** Lifecycle. Registered by calling within a component render */
   onAttach(callback: () => void) {
     console.log('In tree.onAttach()');
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     setLifecycle('onAttach', callback);
   },
   /** Lifecycle. Registered by calling within a component render */
   onDetach(callback: () => void) {
     console.log('In tree.onDetach()');
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
     setLifecycle('onDetach', callback);
   },
 };
@@ -99,11 +141,12 @@ const tree = {
 const setLifecycle = (fn: LifecycleNames, callback: () => void) => {
   const len = tree.cRenderingStack.length;
   if (len === 0) {
-    throw new Error(`Tree: cRenderingStack empty. Can't set ${fn} lifecycle`);
+    throw `tree.cRenderingStack is empty, can't set ${fn} lifecycle`;
   }
   tree.cRenderingStack[len - 1][fn] = callback;
 };
 
+// Unlike other functions this doesn't throw since this needs to keep rendering
 const wrapReviver = (hCall: typeof h) => {
   const wrap: typeof h = (...args: unknown[]) => {
     const [fn] = args;
@@ -135,13 +178,15 @@ const wrapReviver = (hCall: typeof h) => {
       console.error(`${name}: cRenderingStack pop() wrong object: ${String(lifecycles)}`);
       return ret;
     }
+    // TODO: How is DocumentFragment supported?
+    const retTyped = ret as HTMLElement;
     const details: InstanceDetails = {
-      node: ret,
-      attached: false,
+      el: retTyped,
+      isDOMBridge: false,
       ...lifecycles,
     };
-    tree.cInstanceDetails.set(ret, details);
-    tree.createInstance(name, ret);
+    tree.cInstanceDetails.set(retTyped, details);
+    tree.create(name, retTyped);
 
     console.log(`${name}: Done`);
     return ret;
@@ -177,15 +222,24 @@ const trace = (api: SinuousApi) => {
     return insert(...args);
   };
 
-  api.add = (parent: Node, value: string | Node, endMark?: Node) => {
+  api.add = (parent: El, value: string | El, endMark?: El) => {
     console.log(++countAdds, `${type(parent)}\n    <- ${type(value)}`);
-    if (value instanceof Node) {
+    if (value instanceof Node
+     && tree.isComponent(parent)
+     && tree.isComponent(value)
+    ) {
       tree.relate(parent, value);
     }
     return add(parent, value, endMark);
   };
 
   // TODO: api.rm to support onDetach()
+};
+
+const tree = {
+  ...treeDS,
+  ...treeInternalMethods,
+  ...treeLifecycleMethods,
 };
 
 export { tree, trace };
