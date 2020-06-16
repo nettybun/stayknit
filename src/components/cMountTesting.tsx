88e0f71ff25e2bc3709a62b8a2e19509d68519e7
Gen Hames <hamesg@tutanota.de>
2020-06-16 12:33:09 -0700

    Avoid conditional typing entirely since JSX allows returning null
    It's a bit of a loophole, but that's fine

--- src/components/cMountTesting.tsx
+++ src/components/cMountTesting.tsx
@@ -1,32 +1,37 @@
 import { h, observable } from 'sinuous';
 import { tree, ds } from '../trace/index.js';
-import { ObservableCollection } from '../types/index.js';
+
+// XXX: Automate this
+// At least the generic observable splicing, event reg, and tree methods.
+// const liveObservables = C.hydrate();
+// const meta = ds.compMeta.get(el /* From the SSR run / querySelector */);
+// if (!meta) throw `No meta for ${el.tagName}`;
+// meta.observableLocationMarkers.forEach::subscribe(() => api.insert());
 
 // XXX: Not allowed to return an ObservableCollection...
 // Instead of having the hydration function call MyComp.hydrate I might need to
-// pass in a special prop to provide type safety...
-
-// Meaning that <MountTest hydrate> => ObservableCollection (not assignable as a
-// JSX element, which is good). The real call convention would be as a function
-// in the reviver: MountTest({ hydrate: true }) => ObservableCollection
+// pass in a ~~special prop~~ function argument to provide type safety.
+// Considering I'm opinionated about component children, I'd like to type check
+// that all components are void elements (no children) and can change the
+// definition of Component to (props: {}, hydrate)
 
 // A lot of this comes down to functions not being able to inspect their own...
 // self? I suppose it's `this` but () => {} doesn't have `this`
 
-const MountTest: Component = () => {
-  const xhrFetchedCommentCount = observable('...');
-  const windowSize = observable('...');
+const MountTest: HydratableComponent = (_, s = {}) => {
+  s.xhrFetchedCommentCount = observable('...');
+  s.windowSize = observable('...');
 
   const onWindowResize = debounce(() => {
-    windowSize(`${window.innerWidth}px x ${window.innerHeight}px`);
+    s.windowSize(`${window.innerWidth}px x ${window.innerHeight}px`);
   }, 250);
   const fetchController = new AbortController();
   tree.onAttach(() => {
     void fetch('/404', { signal: fetchController.signal })
       .then(r => r.text())
-      .then(count => xhrFetchedCommentCount(count));
+      .then(count => s.xhrFetchedCommentCount(count));
     setTimeout(() => {
-      xhrFetchedCommentCount('50');
+      s.xhrFetchedCommentCount('50');
     }, 500);
     onWindowResize();
     window.addEventListener('resize', onWindowResize);
@@ -37,27 +42,17 @@ const MountTest: Component = () => {
   });
 
   // Short circuit render. Observables will be patched into existing DOM
-  if (window.hydrating)
-    return { xhrFetchedCommentCount, windowSize };
+  if (window.hydrating) return null;
 
   return (
     <div class="bg-gray-300 m-5 p-5 ml-0">
       {/* During SSR these observables are tracked by inserting tagged span tags
           to act as location markers for patching during hydration */}
-      <p>The window's size is <span>{windowSize}</span></p>
-      <p>This post has {xhrFetchedCommentCount} comments</p>
+      <p>The window's size is <span>{s.windowSize}</span></p>
+      <p>This post has {s.xhrFetchedCommentCount} comments</p>
     </div>
   );
 };
-
-MountTest.hydrate = MountTest as unknown as () => ObservableCollection;
-
-// TODO: Automate this
-// At least the generic observable splicing, event reg, and tree methods.
-// const liveObservables = C.hydrate();
-// const meta = ds.compMeta.get(el /* From the SSR run / querySelector */);
-// if (!meta) throw `No meta for ${el.tagName}`;
-// meta.observableLocationMarkers.forEach::subscribe(() => api.insert());
 
 // NOICE.
 function debounce(func: (...args: unknown[]) => void, wait: number, immediate?: boolean) {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,17 @@
 // This is mostly to support SSR
 
 import type { h, JSX } from 'sinuous';
-import type { ElementChildren } from 'sinuous/shared';
 import type { Observable } from 'sinuous/observable';
 
 type JSXProp =
   | JSX.HTMLAttributes &
     Record<string, unknown>
-  | null;
+  | null
 
-type JSXEl = h.JSX.Element;
-type ObservableCollection = { [k: string]: Observable<unknown> }
+// EDIT: This was the wrong type... Components should be able to return null to
+// say they don't have anything. That's common in React
+type JSXEl = h.JSX.Element | null;
+type ObservableRefs = { [k: string]: Observable<unknown> }
 
 declare global {
   interface Window {
@@ -19,33 +20,43 @@ declare global {
   // Component that declare `C.hydrate = C` will return only their state on
   // hydration after SSR
 
-  type Component = {
-    (props: JSXProp, ...children: ElementChildren[]): JSXEl
-    hydrate: (props: JSXProp, ...children: ElementChildren[]) => ObservableCollection
-  }
+  // What about...passing in an object that the component passively writes into
+  // during SSR so I don't need to tap into observable/src at all because
+  // there's reassurance that the used observables will be populated
+
+  // I'll lie to TS a bit. This definition will be for client JS only. In the
+  // server I'll pass in a collection object
+
+  // FIXME: How to change JSX typechecking to forbid children on _all_ components?
+  type HydratableComponent = (props: JSXProp & { children?: never }, s: ObservableRefs) => JSXEl
+
+  // XXX: Why aren't there are type hints for this? Using `props` is `any` type
+  // type Component =
+  //   | ((props: JSXProp, hy?: unknown) => JSXEl)
+  //   | ((props: JSXProp, hy: ObservableRefs) => void)
 
   // XXX: Self-referencing hydrate function
   // XXX: "MyComp can't be used as a JSX element" (as ObservableRefs isn't assignable)
   // type Component =
-  //   | { (props: JSXProp, ...children: ElementChildren[]): JSXEl, hydrate: never }
-  //   | { (props: JSXProp, ...children: ElementChildren[]): JSXEl | ObservableCollection, hydrate: Component }
+  //   | { (props: JSXProp): JSXEl, hydrate: never }
+  //   | { (props: JSXProp): JSXEl | ObservableRefs, hydrate: Component }
 
   // XXX: Requires passing a <T> and MyComp: <typeof MyComp> references itself
   // interface C<T> {
-  //   (props: JSXProp, ...children: ElementChildren[]):
+  //   (props: JSXProp):
   //     T extends { hydrate: C<T> }
-  //       ? JSXEl | ObservableCollection
+  //       ? JSXEl | ObservableRefs
   //       : JSXEl
   // }
 
   // XXX: Can't overload interfaces...
   // interface C {
-  //   (props: JSXProp, ...children: ElementChildren[]): JSXEl
+  //   (props: JSXProp): JSXEl
   // }
   // interface C {
-  //   (props: JSXProp, ...children: ElementChildren[]): JSXEl | { [k: string]: Observable<unknown> }
+  //   (props: JSXProp): JSXEl | { [k: string]: Observable<unknown> }
   //   hydrate: C
   // }
 }
 
-export { ObservableCollection };
+export { JSXProp, JSXEl, ObservableRefs };
