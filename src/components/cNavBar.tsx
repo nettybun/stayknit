72f171bd3aab97fa32e5b02f907827f65636f740
Gen Hames <hamesg@tutanota.de>
2020-06-17 10:44:15 -0700

    Typo

--- src/components/cNavBar.tsx
+++ src/components/cNavBar.tsx
@@ -1,5 +1,5 @@
 import { h } from 'sinuous';
-import { addMessage } from '../state/messages.jsx';
+import { addMessage } from '../state/messages.js';
 
 const NavBar = ({ items }: { items: string[] }): h.JSX.Element =>
   <div className="flex mb-2 border-t border-r border-l text-sm rounded">
