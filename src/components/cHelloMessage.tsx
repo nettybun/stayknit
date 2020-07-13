ef85bed2d45ee204b7b94a851774c0eb4dadfa0c
Gen Hames <hamesg@tutanota.de>
2020-07-12 22:25:12 -0700

    It's working 💅💅💅

--- src/components/cHelloMessage.tsx
+++ src/components/cHelloMessage.tsx
@@ -1,14 +1,25 @@
 import { h, hooks } from '../sinuous.js';
 import { observable } from 'sinuous/observable';
+import { css } from 'style-takeout.macro';
+
+const baseStyle = css`
+  transition-timing-function: ease-in-out;
+  transition-property: background-color;
+  transition-duration: 500ms;
+`;
+
+const orangeStyle = css`
+  background-color: #ff8822;
+`;
 
 const HelloMessage = ({ name }: { name: string }): h.JSX.Element => {
   // I've decided this doesn't need hydration
-  const style = observable('transition-colors duration-500 ease-in-out');
+  const style = observable(baseStyle);
 
   hooks.onAttach(() => {
     // Simulate async call that takes some time...
     setTimeout(() => {
-      style(`${style()} bg-orange-400`);
+      style(`${baseStyle} ${orangeStyle}`);
     }, 100);
   });
   hooks.onDetach(() => {});
