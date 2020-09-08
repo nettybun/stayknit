d4d763b4e21949a99b63bc8f5eef4aec8f44002e
Gen Hames <hamesg@tutanota.de>
2020-09-08 11:02:12 -0700

    No one said she has to be tight

--- src/index.tsx
+++ src/index.tsx
@@ -15,24 +15,36 @@ import { stripIndent } from './util.js';
 
 const Page = () =>
   <main class={`${styles.Page} space`}>
-    <h1 class={css`${snippets.text.xl_4}`}>Hi 🌺</h1>
+    <h1 class={css`font-weight: 400; ${snippets.text.xl_4}`}>Hi 🌺</h1>
+
     <p>This is a testing page for <Link to="https://sinuous.dev" name="Sinuous"/>. You'll need a modern browser. It's all ESM modules and no transpilation. I'm on Firefox 72.</p>
+
     <p>I've added onAttach/onDetach lifecycles for components so they can run code once they're added to the page, even if that's long after they're created. It uses WeakMaps.</p>
+
     <p><strong>Open your browser's console to see the application tracing</strong></p>
+
     <p>The source code is here: <Link to="https://gitlab.com/nthm/sinuous-packages"/>. This testing page has its source here: <Link to="https://gitlab.com/nthm/stayknit"/>.</p>
+
     <p>The packages are pluggable, and I've tried to make it easy to extend the functionality. Some example plugins you could write: gather render timing information; re-render counts; or showing a warning when a component hits a certain number of child elements.</p>
+
     <NavBar items={['Add A', 'Add B', 'Add C', 'Add D', 'Add E']} />
+
     <p>Below the content is rendered as a <code>`when()`</code> block</p>
     {when(() => route(), {
       A: routeA,
       B: routeB,
     })}
+
     <HelloMessage name="This is a <HelloMessage/> component"/>
+
     <p>Below, a list of HelloMessage components are being rendered like this:</p>
+
     <pre class={styles.CodeBlock}>
       {'() => messages().map(x => <p><HelloMessage name={x}/></p>)'}
     </pre>
+
     <p>Each time a message is added, all old components are removed (causes onDetach) and new components are created from scartch and added (onAttach).</p>
+
     <div>{
       when(() => count() === 0 ? '0' : count() < 10 ? '0..10' : '>= 10', {
         '0':     () => <p>There's no messages right now</p>,
@@ -41,6 +53,7 @@ const Page = () =>
       })
     }
     </div>
+
     <button
       class={styles.ButtonBlue}
       type="button"
@@ -48,6 +61,7 @@ const Page = () =>
     >
       Add message
     </button>
+
     <div>
       {() => messages().map(x => <p><HelloMessage name={x}/></p>)}
     </div>
@@ -68,10 +82,15 @@ const routeA = () =>
     >
       Remove blue dashed content via <code>`when()`</code>
     </button>
+
     <p>There's {() => `${count()} message${count() === 1 ? '' : 's'}`} right now</p>
+
     <LoginForm />
+
     <p>This component below will be removed after 5 messages are in the list</p>
+
     <p>This is the logic:</p>
+
     <pre class={styles.CodeBlock}>{stripIndent(`
       when(() => count() < 5 ? 'T' : 'F', {
         T: () => <span><AttachTest/></span>,
